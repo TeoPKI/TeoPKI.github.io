0a936f63d3d678f60dd5878af2e63ca62409c79a
TeoPKI <teo.peltokorpi@gmail.com>
2022-01-20 08:35:26 +0200

    first

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function openTab(tabId) {
+    var tabs;
+    tabs = document.getElementsByClassName("tab");
+
+    for (let index = 0; index < tabs.length; index++) {
+        const element = tabs[index];
+        element.style.display = "none";
+    }
+
+    document.getElementById(tabId).style.display = "block";
+}
